646909172f839913440d22706a8065b92cd3f44d
ROCKTAKEY <rocktakey@gmail.com>
2023-02-21 01:56:15 +0900

    Initial commit

---
+++ dummy-example.el
@@ -0,0 +1,38 @@
+;;; dummy-example.el --- Dummy project  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: tools
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/ROCKTAKEY/dummy-example-el
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Dummy project
+
+;;; Code:
+
+(defgroup dummy-example ()
+  "Dummy project."
+  :group 'tools
+  :prefix "dummy-example-"
+  :link '(url-link "https://github.com/ROCKTAKEY/dummy-example-el"))
+
+(provide 'dummy-example)
+;;; dummy-example.el ends here
